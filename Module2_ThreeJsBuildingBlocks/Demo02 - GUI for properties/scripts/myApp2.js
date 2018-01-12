c2a97ec9c81218171251981f34aa0ad2c5193a5f
YonePC <YonePC@YONE>
2018-01-12 16:06:00 +0000

    Added camera

--- Module2_ThreeJsBuildingBlocks/Demo02 - GUI for properties/scripts/myApp2.js
+++ Module2_ThreeJsBuildingBlocks/Demo02 - GUI for properties/scripts/myApp2.js
@@ -12,5 +12,22 @@ var myClass = (function () {
         plane,
         ground;
 
+    function initScene() {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById("webgl-container").appendChild(renderer.domElement);
 
+        scene.add(light);
+
+        camera = new THREE.PerspectiveCamera(
+            35,
+            window.innerWidht / window.innerHeight,
+            1,
+            1000
+        );
+
+        camera.position.z = 100;
+        scene.add(camera);
+
+
+    }
 })();
