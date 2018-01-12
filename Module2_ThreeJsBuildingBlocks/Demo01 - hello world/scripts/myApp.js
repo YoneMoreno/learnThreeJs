e57f2b148110c9443fb86ed8271a4d55cd58d112
YonePC <YonePC@YONE>
2018-01-12 15:19:32 +0000

    Created my own ThreeJS scene

---
+++ Module2_ThreeJsBuildingBlocks/Demo01 - hello world/scripts/myApp.js
@@ -0,0 +1,12 @@
+var myClass = (function(){
+
+    "use strict";
+
+    var scene=new THREE.Scene(),
+        renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
+        light = new THREE.AmbientLight(0xffffff),
+        camera,
+        box;
+
+
+})();
