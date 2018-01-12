ec7d7a2dae4c838c5f92489bc3728ede91f21784
YonePC <YonePC@YONE>
2018-01-12 15:28:45 +0000

    added camera to scene

--- Module2_ThreeJsBuildingBlocks/Demo01 - hello world/scripts/myApp.js
+++ Module2_ThreeJsBuildingBlocks/Demo01 - hello world/scripts/myApp.js
@@ -1,14 +1,29 @@
-var myClass = (function(){
+var myClass = (function () {
 
     "use strict";
 
-    var scene=new THREE.Scene(),
+    var scene = new THREE.Scene(),
         renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
         light = new THREE.AmbientLight(0xffffff),
         camera,
         box;
 
-    function initScene(){
+    function initScene() {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById("webgl-container").appendChild(renderer.domElement);
+
+
+        scene.add(light);
+
+        camera = new THREE.PerspectiveCamera(
+            35,
+            window.innerWidth / window.innerHeight,
+            1,
+            1000
+        );
+
+        camera.position.z = 100;
+        scene.add(camera);
     }
 
 })();
