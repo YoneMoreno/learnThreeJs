331e486769da0839515d0d54496b48fc8b6fff98
YonePC <YonePC@YONE>
2018-01-12 16:03:34 +0000

    Second example scene

---
+++ Module2_ThreeJsBuildingBlocks/Demo02 - GUI for properties/scripts/myApp2.js
@@ -0,0 +1,16 @@
+var myClass = (function () {
+    "use strict";
+
+    var scene = new THREE.Scene(),
+        light = new THREE.AmbientLight(0xffffff),
+        light2 = new THREE.AmbientLight(0xffffff),
+        camera,
+        renderer = new THREE.WebGLRenderer(),
+        cube,
+        cube2,
+        cube3,
+        plane,
+        ground;
+
+
+})();
